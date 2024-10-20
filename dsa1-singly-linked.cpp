e3ca58c87a2c85f05559658ad3cda9c3e17080a9
JEJO J <147123805+jejo205713@users.noreply.github.com>
2024-10-20 17:03:48 +0530

    Update dsa1-singly-linked.cpp

--- dsa1-singly-linked.cpp
+++ dsa1-singly-linked.cpp
@@ -1,50 +1,44 @@
 //EXP 1:
-//implementation of singly linked list : 
-
+//implementation of : SINGLEY LINKED LIST : 
 #include<bits/stdc++.h>
 using namespace std;
 struct node{
     int data;
-    node *next;
+    node * next;
 };
-node *head=NULL;
-void insert(int num)
-{
-    node *newnode=new node();
-    newnode->data=num;
-    if(head==NULL)
-    {
-    newnode->next=head;
-    head=newnode;
+node* head=NULL;
+
+void append(int num){
+    node* newnode=new node();
+    newnode->data=num; //*i keep forgetting assign the input valur to newnode->data;
+    if(head==nullptr){
+        newnode->next=head;
+        head=newnode;
     }
     else{
-    node*temp=head;
-    while(temp->next!=NULL)
-    {
-        temp=temp->next;
+        node* temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+        newnode->next=NULL;
     }
-    temp->next=newnode;
-    newnode->next=NULL;
-}
 }
 
 void display(){
-    node*temp=head;
-    while(temp!=0)
-    {
+    node* temp=head;
+    while(temp!=0){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
 }
-int main(){
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int num;
-        cin>>num;
-        insert(num);
-    
-}
-display();
-    
-}
+ int main(){
+     int n;
+     cin>>n;
+     for(int i=0;i<n;i++){
+         int num;
+         cin>>num;
+         append(num);
+     }
+     display();
+ }
